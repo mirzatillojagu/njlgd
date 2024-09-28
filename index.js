f2d1cddf4f4cdff5a0b7c2adf1f96659c4c70784
mirzatillojagu <imirzatillo0@gmail.com>
2024-09-28 18:17:05 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,322 @@
+// 1
+// let a  = +prompt("sonni kriting")
+
+// if ( a > 0) {
+//     ++a;
+// }
+// console.log(a);
+
+
+// 2
+// let a  = +prompt("sonni kriting")
+
+// if ( a > 0) {
+//     ++a;
+// }else {
+//     a = a - 2;
+// }
+
+// console.log(a);
+
+// 3
+// let a  = +prompt("sonni kriting")
+
+// if ( a > 0) {
+//     ++a;
+// } else if (a == 0) {
+//     a = 10;
+// }else{
+//     a = a - 2;
+// }
+
+// console.log(a);
+
+
+// 4
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+// let c = +prompt("3son")
+
+// let count =0;
+
+// if (a > 0 ) {
+//     count++;
+// }
+
+// if (b > 0) {
+//     count++;
+// }
+
+// if (c > 0) {
+//     count++;
+// }
+
+// console.log(count);
+
+
+
+// 5
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+// let c = +prompt("3son")
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0 ) {
+//     musbat++;
+// }else{
+//     manfiy++;
+// }
+
+// if (b > 0) {
+//     musbat++;
+// }else{
+//     manfiy++;
+// }
+
+
+// if (c > 0) {
+//     musbat++;
+// }else{
+//     manfiy++;
+// }
+
+
+// console.log(musbat, manfiy);
+
+// 6
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+
+// if (a > b) {
+//     max = a;
+// }else{
+//     max = b;
+// }
+
+// console.log(max);
+
+// 7
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+// let min=0;
+
+// if (a > b) {
+//     max = a;
+//     min = b;
+// }else{
+//     max = b;
+//     min = a;
+// }
+
+// console.log(min, max);
+
+// 8
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max=0;
+// let min=0;
+
+// if (a > b) {
+//     max = a;
+//     min = b;
+// }else{
+//     max = b;
+//     min = a;
+// }
+
+// console.log(max, min);
+
+
+
+// 10
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let res = 0;
+
+// if (a == b) {
+//     res = a + b;
+// }else{
+//     res = 0;
+// }
+
+// console.log(res);
+
+// 11
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+
+// let max = 0;
+
+// if (a > b ) {
+//     max = a;
+// }else if (a < b) {
+//     max = b;
+// }else{
+//     max = 0;
+// }
+
+// console.log(max);
+
+
+// 12
+
+// let a = +prompt("1son")
+// let b = +prompt("2son")
+// let c = +prompt("3son")
+
+// let min = 0;
+
+// if (a > b) {
+//     min = b;
+// }else{
+//     min = a;
+// }
+
+// if (b > c) {
+//     min = c
+// }
+
+// console.log(min);
+
+
+
+
+
+
+
+
+//////////////////////BOOLEN/////////////////////////////////
+
+
+//1-misol
+// let num = +prompt(sonni kiriting)
+
+// if(num>0){
+//     console.log(musbat son);
+// }else{
+//     console.log(manfiy son);
+    
+// }
+
+
+//2-misol
+// let num =+prompt(sonni kiriting)
+
+// if (num % 2 == 1) {
+//     console.log(toq son);
+    
+// }else{
+//     console.log(toq son emas);
+    
+// }
+
+
+//3-misol
+
+// let num =+prompt(sonni kiriting)
+
+// if (num % 2 == 0) {
+//     console.log(juft son);
+    
+// }else{
+//     console.log(juft son emas);
+    
+// }
+
+
+//4-misol
+
+// let a =+prompt(1-sonni kiriting)
+// let b =+prompt(2-sonni kiriting)
+
+// if(a>2 && b<=3){
+//     console.log(jumla togri);
+    
+// }else{
+//     console.log(jumla notogri);
+// }
+
+//5-misol
+// let a =+prompt(1-sonni kiriting)
+// let b =+prompt(2-sonni kiriting)
+
+// if(a >= 0  b < -2){
+//     console.log(`jumla togri`);
+    
+// }else{
+//     console.log(`jumla notogri`);
+// }
+
+//6-misol
+//  let a =+prompt(`1-sonni kiriting`)
+// let b =+prompt(`2-sonni kiriting`)
+// let c =+prompt(`3-sonni kiriting`)
+
+// if(a<=b && b<=c){
+//     console.log(`jumla togri`);
+// }else{
+//     console.log(`jumla notogri`);
+// }
+
+
+//7-misol
+
+// let a =+prompt(`1-sonni kiriting`)
+//  let b =+prompt(`2-sonni kiriting`)
+//  let c =+prompt(`3-sonni kiriting`)
+
+//  if ((b > a && b < c)  (b > c && b < a)) {
+//     console.log("b soni a va c orasida yotadi");
+// } else {
+//     console.log("b soni a va c orasida yotmaydi");
+// }
+
+//8-misol
+// let a =+prompt(1-sonni kiriting)
+//  let b =+prompt(2-sonni kiriting)
+
+//  if(a % 2 == 1 && b % 2 == 1 ){
+// console.log(toq sonlar );
+
+//  }else{
+//     console.log(toq sonlar emas);
+//  }
+
+
+//9-misol
+
+// let a =+prompt(1-sonni kiriting)
+//   let b =+prompt(2-sonni kiriting)
+
+//   if(a % 2 == 1  b % 2 == 1 ){
+//  console.log(`toq son `);
+
+//  }else{
+//     console.log(`toq son emas`);
+// }
+
+//10-misol
+
+// let a =+prompt(`1-sonni kiriting`)
+// let b =+prompt(`2-sonni kiriting`)
+
+// if(a % 2 == 1 && b % 2 == 0  a % 2 == 0 && b % 2 == 1 ){
+//  console.log(jumla togri );
+
+// }else{
+//     console.log(jumla notogri);
+//  }
+
+
